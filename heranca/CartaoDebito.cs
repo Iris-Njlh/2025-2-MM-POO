e6bb86fc8f603414358b72fbca6888c4047b4643
Iris-Njlh <iris.lima@uni9.edu.br>
2025-09-16 00:03:32 +0000

    Pastas aula Hernça POO 15/09

--- heranca/CartaoDebito.cs
+++ heranca/CartaoDebito.cs
@@ -8,6 +8,9 @@ public class CartaoDebito
     public string Numero { get; set; }
     //publi string Bandeira {get; set;} //visa, Visa, VISA
     public BandeiraCartao Bandeira { get; set; }
+    public DateTime Vencimento { get; set; }
+    public string Portador { get; set; }
+    public string CVV { get; set; }
 }
 
 public enum BandeiraCartao
