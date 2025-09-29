dc9f557d2dec6710f56d715d3d41234fddbf4fb1
Iris-Njlh <iris.lima@uni9.edu.br>
2025-09-29 23:10:09 +0000

    cartão debito

--- heranca/CartaoDebito.cs
+++ heranca/CartaoDebito.cs
@@ -2,15 +2,39 @@ public class CartaoDebito
 {
     public CartaoDebito()
     {
-        Bandeira = BandeiraCartao.Mastercard;
+        Bandeira = BandeiraCartao.Visa;
     }
 
     public string Numero { get; set; }
-    //publi string Bandeira {get; set;} //visa, Visa, VISA
     public BandeiraCartao Bandeira { get; set; }
     public DateTime Vencimento { get; set; }
     public string Portador { get; set; }
-    public string CVV { get; set; }
+    public string Cvv { get; set; }
+
+    public decimal Saldo { get; set; }
+    public decimal Limite { get; set; }
+
+    public bool Debitar(decimal valor)
+    {
+        if (valor > Saldo)
+            return false;
+
+        Saldo -= valor;
+        return true;
+    }
+
+    public bool Debitar(decimal valor, bool usarLimite)
+    {
+        if (usarLimite)
+        {
+            if(valor <= (Saldo + Limite))
+                return Debitar(valor);
+            else
+                return false;
+        }
+
+        return Debitar(valor);
+    }
 }
 
 public enum BandeiraCartao
