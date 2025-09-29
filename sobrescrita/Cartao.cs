d3f1cf6b63284f2532b9c985cf732c2cd6c983b0
Iris-Njlh <iris.lima@uni9.edu.br>
2025-09-29 23:56:44 +0000

    aleatorio

---
+++ sobrescrita/Cartao.cs
@@ -0,0 +1,7 @@
+public class Cartao
+{
+    public void Debitar(decimal valor)
+    {
+        Console.WriteLine($"Débito de {valor:C} realizado no cartão.");
+    }
+}
---
+++ sobrescrita/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Cartao cartaoBase = new Cartao();
+cartaoBase.Debitar(1234567891011);
